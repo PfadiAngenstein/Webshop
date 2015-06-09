8fd0f3ebe37c8be0ba566cf1c3779af4db06a2e3
faitNOISE <faitnoise@users.noreply.github.com>
2015-06-09 15:18:58 +0200

    Create danke.php

---
+++ php/danke.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="ISO-8859-4">
+	<meta name="theme-color" content="#FFEB3B">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<title>Warenkorb - Pfadi Angenstein</title>
+	<link rel="stylesheet" type="text/css" href="style/normalize.css">
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="stylesheet" type="text/css" href="style/colorbox.css">
+</head>
+
+<body>
+
+<div class="banner">
+	<div class="content">
+	 <a href="index.php"><h1><img src="images/angenstein.gif" alt="Logo Pfadi Angenstein">&nbsp;Warenkorb</h1></a>
+	 </div>
+</div>
+
+<div id="products-wrapper">
+
+ <div class="view-cart">
+ 	<h2>Bestellung wurde versendet!</h2>
+ 	<p>Besten Dank für Ihre Bestellung.</p>
+ 	<p>Bei Fragen zu Ihrer Bestellung wenden Sie sich an <a href="mailto:shop@pfadiangenstein.ch">shop@pfadiangenstein.ch</a>.</p>
+ 	<p>Der Verantwortliche des Shops wird sich bei Ihnen melden.</p>
+	<p>Gehen Sie zurück auf <a href="http://www.pfadiangenstein.ch">www.pfadiangenstein.ch</a>.</p>
+</div>
+
+</div>
+
+<div class="clear"></div>
+<footer>
+	<p>&copy; Pfadi Angenstein</p>
+</footer>
+
+</body>
+</html>
